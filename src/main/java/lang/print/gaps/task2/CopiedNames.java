c842c6d8496d1d5646f0b1d784dc8b0cf5ab9de1
elmiguel0 <miguelangel.ramos@ibm.com>
2022-11-13 20:40:12 -0600

    Validate naming conventions

--- src/main/java/lang/print/gaps/task2/CopiedNames.java
+++ src/main/java/lang/print/gaps/task2/CopiedNames.java
@@ -2,13 +2,13 @@ package lang.print.gaps.task2;
 
 public class CopiedNames {
     public static void main(String[] args) {
-        int variable = 0;
-        int variable = 2000;
-        int value = 1000;
-        int variable = 4000;
-        int value2 = 3000;
-        int variables = 5000;
-        variables = 7000;
-        int value = 90000;
+        int zero = 0;
+        int two_thousand = 2000;
+        int one_thousand = 1000;
+        int four_thousand = 4000;
+        int three_thousand = 3000;
+        int five_thousand = 5000;
+        int seven_thousand = 7000;
+        int nine_thousand = 90000;
     }
 }
